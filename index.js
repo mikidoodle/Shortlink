7e45ca343d1df83b691c77821b87d91a8215acaa
Mihir Maroju <mihir@pidgon.com>
2025-02-21 15:49:21 +0530

    whats all that movement back there 🧐

--- index.js
+++ index.js
@@ -12,32 +12,7 @@ const router = express.Router();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', function(req, res) {
- res.send(`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <link href="https://pidgon.com/style.css" rel="stylesheet" type="text/css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>${info.get('name')}</title>
-</head>
-<body align="center">
-var path = require('path');
-const shortid = require('shortid');
-var PORT = 3000;
-const { v4: uuidv4 } = require('uuid');
-const Database = require("easy-json-database");
-const links = new Database('./links.json')
-const info = new Database('./INFO')
-const fs = require('fs')
-const router = express.Router();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', function(req, res) {
- res.send(`<!DOCTYPE html>
+res.send(`<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
